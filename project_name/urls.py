db0f4ad0155fa685d7230920534f5a4a24dc0ccc
Ryan Montoya <rcmontoy@gmail.com>
2019-08-14 13:48:21 -0400

    Adds placeholder index view and route

--- project_name/urls.py
+++ project_name/urls.py
@@ -2,7 +2,10 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
+from .views import index
+
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
 ]
 
